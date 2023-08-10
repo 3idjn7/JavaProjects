a9f95298ca9f664d57379e73bdffea0c1f7f59a3
3idjn7 <seprefy@gmail.com>
2023-08-10 15:48:25 +0300

    MobileBgCrawler scraping all ads, just need to work out the kinks of not posting in cyrillic.

--- CrawlingMobileBG/src/main/java/MobileBgCrawler.java
+++ CrawlingMobileBG/src/main/java/MobileBgCrawler.java
@@ -1,8 +1,7 @@
-/*import org.jsoup.Jsoup;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,58 +11,46 @@ import java.sql.SQLException;
 public class MobileBgCrawler {
 
     public static void main(String[] args) {
-        String url = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=td61bn&f1=1";
-
         String dbUrl = "jdbc:mysql://localhost:3306/car_ads_db";
         String dbUser = "root";
         String dbPassword = "databasepassword";
 
         try {
-            Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+            String url = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=td61bn&f1=2";
 
             Document document = Jsoup.connect(url).get();
-            Elements offerItems = document.select(".mmm"); // Selecting ad title elements
+            Elements adElements = document.select("table.tablereset");
 
-            for (Element offerItem : offerItems) {
-                Element titleElement = offerItem.selectFirst("a.mmm");
-                String title = titleElement.text();
+            for (Element adElement : adElements) {
+                Element titleElement = adElement.selectFirst("td.valgtop a.mmm");
+                Element priceElement = adElement.selectFirst("td.algright.valgtop span.price");
 
-                Element priceElement = offerItem.selectFirst(".price");
-                String price = priceElement.text();
+                if (titleElement != null && priceElement != null) {
+                    String title = titleElement.text();
+                    String price = priceElement.text();
 
-                String make = "Mercedes-Benz"; // Since we're targeting Mercedes-Benz
+                    // Extract make and model from the title
+                    String[] titleParts = title.split(" ", 2);
+                    String make = titleParts[0];
+                    String model = titleParts.length > 1 ? titleParts[1] : "";
 
-                int indexOfHyphen = title.indexOf("-");
-                if (indexOfHyphen != -1) {
-                    String model = title.substring(indexOfHyphen + 1).trim();
-
-                    System.out.println("Title: " + title);
-                    System.out.println("Price: " + price);
-                    System.out.println("Model: " + model);
-
-                    insertAdData(connection, make, model, price);
-                } else {
-                    System.out.println("Hyphen not found in title: " + title);
+                    // Store data in the database
+                    try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+                        String sql = "INSERT INTO car_ads (make, model, price) VALUES (?, ?, ?)";
+                        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                            statement.setString(1, make);
+                            statement.setString(2, model);
+                            statement.setString(3, price);
+                            statement.executeUpdate();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-
-            connection.close();
-            System.out.println("Ads inserted into the database successfully.");
-        } catch (IOException | SQLException e) {
+            System.out.println("Data scraping and storing completed successfully.");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    private static void insertAdData(Connection connection, String make, String model, String price) throws SQLException {
-        System.out.println("Price extracted from ad: " + price);
-
-        String insertQuery = "INSERT INTO car_ads (make, model, price) VALUES (?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-            preparedStatement.setString(1, make);
-            preparedStatement.setString(2, model);
-            preparedStatement.setString(3, price);
-            preparedStatement.executeUpdate();
-        }
-    }
 }
-*/
