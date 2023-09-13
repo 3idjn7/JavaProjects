51dc6c8a9eb34e158cbf612d4bf800137ec70095
3idjn7 <seprefy@gmail.com>
2023-09-14 01:35:13 +0300

    Finally fixed the AdsProcessor version of the scraper! Broke down the processAds method into two methods. -Before it ran through the whole logic of scraping all ads then checking if there are repeats adding to an object list and THEN adding to the database, quite dumb not to have figured it out way earlier but ok. -Now checks if database is empty and runs through the empty method that scrapes everything and ads to database, else if the database isn't empty it runs through the other method that checks for duplicates and only ads missing ads.

--- CrawlingMobileBG/src/main/java/AdsProcessor.java
+++ CrawlingMobileBG/src/main/java/AdsProcessor.java
@@ -15,121 +15,160 @@ import static Utilities.ThreadManager.THREAD_COUNT;
 
 public class AdsProcessor {
 
-
     private static final AtomicInteger totalAdsProcessed = new AtomicInteger(0);
     private static final AtomicInteger totalAdsAdded = new AtomicInteger(0);
+    private static final AtomicInteger totalAdsUpdated = new AtomicInteger(0);
 
     public static void main(String[] args) {
         System.out.println("Accessing database.");
         Properties properties = DatabaseUtility.loadDatabasePropertiesFromClasspath("config.properties");
-        processAds(properties);
-    }
-
-    public static void processAds(Properties properties) {
         boolean isDatabaseNew = isDatabaseEmpty(properties);
 
+        if (isDatabaseNew) {
+            processAdsWhenDatabaseIsEmpty(properties);
+        } else {
+            processAdsWhenDatabaseIsNotEmpty(properties);
+        }
+    }
 
+    public static void processAdsWhenDatabaseIsEmpty(Properties properties) {
         int totalPages = WebUtility.getTotalPages();
-        System.out.println("Total pages to process: " + totalPages);
 
         System.out.println("Activating " + THREAD_COUNT + " threads for maximum warp speed...");
 
         ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
         List<Future<List<AdListing>>> futures = new ArrayList<>();
 
-        WebUtility.processPages(totalPages, url -> futures.add(executor.submit(() -> {
-            try {
-                System.out.println("Processing page: " + url);
-                Document document = Jsoup.connect(url).execute().charset("UTF-8").parse();
+        for (int i = 1; i <= totalPages; i++) {
+            final int page = i;
+            futures.add(executor.submit(() -> {
+                int addedAdsCount = 0;
+                try {
+                    String url = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=tqzxar&f1=" + page;
+                    System.out.println("Processing page: " + url);
+                    Document document = Jsoup.connect(url).execute().charset("UTF-8").parse();
 
-                List<AdListing> adListings = PageScraper.scrapeAdsFromPage(document);
-                try (Connection connection = DatabaseUtility.getConnection(properties)) {
-                    for (AdListing adListing : adListings) {
-                        String adTitle = adListing.title();
-                        String[] titleParts = adTitle.split(" ", 2);
-                        String make = titleParts[0];
-                        String model = titleParts.length > 1 ? titleParts[1] : "";
-                        totalAdsProcessed.incrementAndGet();
+                    List<AdListing> adListings = PageScraper.scrapeAdsFromPage(document);
+                    try (Connection connection = DatabaseUtility.getConnection(properties)) {
+                        for (AdListing adListing : adListings) {
+                            String adTitle = adListing.title();
+                            String[] titleParts = adTitle.split(" ", 2);
+                            String make = titleParts[0];
+                            String model = titleParts.length > 1 ? titleParts[1] : "";
 
-                        if (isAdExistsInDatabase(connection, make, model)) {
-                            String insertSql = isDatabaseNew ?
-                                    "INSERT INTO car_ads (make, model, price) VALUES (?, ?, ?)" :
-                                    "INSERT INTO car_ads (make, model, price, new_flag) VALUES (?, ?, ?, ?)";
-                            totalAdsAdded.incrementAndGet();
-                            System.out.println("Added ad: " + adTitle);
-
+                            String insertSql = "INSERT INTO car_ads (make, model, price) VALUES (?, ?, ?)";
                             try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
                                 statement.setString(1, make);
                                 statement.setString(2, model);
                                 statement.setString(3, adListing.price());
-
-                                if (!isDatabaseNew) {
-                                    statement.setBoolean(4, true); // Set new_flag to true for new ads
+                                int rowsAffected = statement.executeUpdate();
+                                if (rowsAffected > 0) {
+                                    addedAdsCount++;
                                 }
-
-                                statement.executeUpdate();
                             }
                         }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
 
-                return adListings;
-            } catch (IOException e) {
-                System.err.println("Error connecting to URL: " + e.getMessage());
-                return Collections.emptyList();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return Collections.emptyList();
-            }
-        })));
+                    totalAdsProcessed.addAndGet(adListings.size());
+                    totalAdsAdded.addAndGet(addedAdsCount);
+
+                    return adListings;
+                } catch (IOException e) {
+                    System.err.println("Error connecting to URL: " + e.getMessage());
+                    return Collections.emptyList();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return Collections.emptyList();
+                }
+            }));
+        }
 
         executor.shutdown();
 
-        List<AdListing> adsToAdd = new ArrayList<>();
-        for (Future<List<AdListing>> future : futures) {
-            try {
-                adsToAdd.addAll(future.get());
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        if (!adsToAdd.isEmpty()) {
-            try (Connection connection = DatabaseUtility.getConnection(properties)) {
-                String insertSql = isDatabaseNew ?
-                        "INSERT INTO car_ads (make, model, price) VALUES (?, ?, ?)" :
-                        "INSERT INTO car_ads (make, model, price, new_flag) VALUES (?, ?, ?, ?)";
-
-                try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
-                    for (AdListing adListing : adsToAdd) {
-                        String adTitle = adListing.title();
-                        String[] titleParts = adTitle.split(" ", 2);
-                        String make = titleParts[0];
-                        String model = titleParts.length > 1 ? titleParts[1] : "";
-
-                        if (isAdExistsInDatabase(connection, make, model)) {
-                            statement.setString(1, make);
-                            statement.setString(2, model);
-                            statement.setString(3, adListing.price());
-
-                            if (!isDatabaseNew) {
-                                statement.setBoolean(4, true); // Set new_flag to true for new ads
-                            }
-
-                            statement.executeUpdate();
-                        }
-                    }
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
         System.out.println("Ads processing completed.");
+        System.out.println("Total pages processed: " + totalPages);
         System.out.println("Total ads processed: " + totalAdsProcessed);
         System.out.println("Total ads added: " + totalAdsAdded);
 
+    }
+
+    public static void processAdsWhenDatabaseIsNotEmpty(Properties properties) {
+        int totalPages = WebUtility.getTotalPages();
+
+        System.out.println("Activating " + THREAD_COUNT + " threads for maximum warp speed...");
+
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
+        List<Future<List<AdListing>>> futures = new ArrayList<>();
+
+        for (int i = 1; i <= totalPages; i++) {
+            final int page = i;
+            futures.add(executor.submit(() -> {
+                int updatedAdsCount = 0;
+                try {
+                    String url = "https://www.mobile.bg/pcgi/mobile.cgi?act=3&slink=tqzxar&f1=" + page;
+                    System.out.println("Processing page: " + url);
+                    Document document = Jsoup.connect(url).execute().charset("UTF-8").parse();
+
+                    List<AdListing> adListings = PageScraper.scrapeAdsFromPage(document);
+                    try (Connection connection = DatabaseUtility.getConnection(properties)) {
+                        for (AdListing adListing : adListings) {
+                            String adTitle = adListing.title();
+                            String[] titleParts = adTitle.split(" ", 2);
+                            String make = titleParts[0];
+                            String model = titleParts.length > 1 ? titleParts[1] : "";
+
+                            if (ifAdsExistsInDatabase(connection, make, model)) {
+                                String insertSql = "INSERT INTO car_ads (make, model, price, new_flag) VALUES (?, ?, ?, ?)";
+                                try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
+                                    statement.setString(1, make);
+                                    statement.setString(2, model);
+                                    statement.setString(3, adListing.price());
+                                    statement.setBoolean(4, true); // Set new_flag to true for new ads
+                                    int rowsAffected = statement.executeUpdate();
+                                    if (rowsAffected > 0) {
+                                        updatedAdsCount++;
+                                    }
+                                }
+                            }
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                    totalAdsProcessed.addAndGet(adListings.size());
+                    totalAdsUpdated.addAndGet(updatedAdsCount);
+
+                    return adListings;
+                } catch (IOException e) {
+                    System.err.println("Error connecting to URL: " + e.getMessage());
+                    return Collections.emptyList();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return Collections.emptyList();
+                }
+            }));
+        }
+
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Ads processing completed.");
+        System.out.println("Total pages processed: " + totalPages);
+        System.out.println("Total ads processed: " + totalAdsProcessed);
+        System.out.println("Total ads updated: " + totalAdsUpdated);
     }
 
     private static boolean isDatabaseEmpty(Properties properties) {
@@ -148,7 +187,7 @@ public class AdsProcessor {
         return false;
     }
 
-    private static boolean isAdExistsInDatabase(Connection connection, String make, String model) {
+    private static boolean ifAdsExistsInDatabase(Connection connection, String make, String model) {
         String sql = "SELECT COUNT(*) FROM car_ads WHERE make = ? AND model = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, make);
